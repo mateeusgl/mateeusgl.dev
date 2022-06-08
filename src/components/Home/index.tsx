c59d32d5b667420acce0a3793c4966a70284241a
mateeusgl <magolopes20@gmail.com>
2022-06-08 16:30:33 -0300

    :tada: update photo home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { Container, Content } from "./style";
 
+interface UserAvatar {
+	avatar_url: string
+}
+
+
 export function Home(){
+	const [useAvatar, setUseAvatar] = useState<UserAvatar>()
+
+	useEffect(() => {
+		const user = async () => {
+			await fetch("https://api.github.com/users/mateeusgl")
+			.then(response => response.json())
+			.then(data => setUseAvatar(data))
+		}
+
+		user()
+	}, [])
 	return (
 		<Container>
 			<Content>
-				<img src="https://github.com/mateeusgl.png" alt="Foto de Matheus Lopes" />
+				<img src={useAvatar?.avatar_url} alt="Foto de Matheus Lopes" />
 				<p>Olá, aqui é o <br /><span>Mateus Lopes.</span><br /> Desenvolvedor front-end</p>
 				<a href="https://linkedin.com/in/mateeusgl" target="_blank">Linkedin</a>
 			</Content>
