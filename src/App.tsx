340ef85b0912948b67fa3549d4acc8a9a3f7734b
mateeusgl <magolopes20@gmail.com>
2022-06-08 18:40:44 -0300

    :tada: update Skins -> Skills

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { ListProjects } from "./components/ListProjects";
 import { Skills } from "./components/Skills";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,6 +1,6 @@
 import { Container, Content } from "./style";
 
-const skins = [
+const skills = [
 	{
 		image: <svg role="img" viewBox="0 0 24 24" fill="#E34F26" xmlns="http://www.w3.org/2000/svg"><title>HTML5</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/></svg>,
 		name: "HTML"
@@ -35,16 +35,16 @@ const skins = [
 	},
 ]
 
-export function Skins() {
+export function Skills() {
 	return (
 		<Container>
 			<Content>
 				<h1>Habilidades</h1>
 				<ul>
-					{skins.map(skin => (
-						<li key={skin.name}>
-							{skin.image}
-							<p>{skin.name}</p>
+					{skills.map(skill => (
+						<li key={skill.name}>
+							{skill.image}
+							<p>{skill.name}</p>
 						</li>
 					))}
 				</ul>
